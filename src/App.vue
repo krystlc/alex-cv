269b0be5d0946c4bdb8f5b7470ce8d572f92b669
Alex <alex.valle@gmail.com>
2023-02-02 13:37:08 -0500

    added transition

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <KeepAlive> <component :is="currentView" /></KeepAlive>
+  <KeepAlive>
+    <transition name="fade-right"><component :is="currentView" /></transition
+  ></KeepAlive>
 </template>
 
 <script lang="ts" setup>
@@ -15,8 +17,39 @@ const routes = {
 const currentPath = ref(window.location.hash);
 const currentView = computed(() => routes[currentPath.value.slice(1) || "/"]);
 
-window.addEventListener(
-  "hashchange",
-  () => (currentPath.value = window.location.hash)
-);
+window.addEventListener("hashchange", () => {
+  currentPath.value = window.location.hash;
+  scrollTo({ top: 0, left: 0 });
+});
 </script>
+
+<style scoped>
+.fade-right-enter-active {
+  animation: fade-right-in 0.6s ease-out;
+}
+.fade-right-leave-active {
+  animation: fade-right-out 0.3s ease-out;
+}
+
+@keyframes fade-right-in {
+  from {
+    opacity: 0;
+    transform: translateX(10px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+}
+
+@keyframes fade-right-out {
+  from {
+    opacity: 1;
+    transform: translateX(0);
+  }
+  to {
+    opacity: 0;
+    transform: translateX(10px);
+  }
+}
+</style>
