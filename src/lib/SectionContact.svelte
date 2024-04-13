f824fdcfe2216a0cb3702d05c30aecb312a766e4
krystlc <alex.valle@gmail.com>
2024-04-13 02:24:05 -0400

    feat: remove map

--- src/lib/SectionContact.svelte
+++ src/lib/SectionContact.svelte
@@ -2,39 +2,21 @@
   import SectionTitle from "./SectionTitle.svelte";
 </script>
 
-<div>
-  <section class="max-w-2xl space-y-8">
-    <SectionTitle
-      title="Contact"
-      subtitle="Reach out to connect, collaborate, and create together"
-    />
-    <div class="grid md:grid-cols-2 gap-x-16">
-      <p>
-        <strong>Name:</strong> Alex Valle<br /><strong>Status:</strong>
-        US Citizen<br /><strong>Location:</strong>
-        Tampa, FL
-      </p>
-      <p class="overflow-hidden text-ellipsis">
-        <strong>Clearance:</strong> Secret<br /><strong>Email:</strong>
-        alex.valle@gmail.com<br /><strong>Languages:</strong>
-        English, Español
-      </p>
-    </div>
-  </section>
-  <div class="py-8 md:py-16">
-    <div>
-      <a
-        href="https://maps.app.goo.gl/gT2vDgTimcv8yVwm6"
-        target="_blank"
-        rel="nofollow"
-        class="h-48 block"
-      >
-        <img
-          src="/map.png"
-          alt="Tampa, FL"
-          class=" object-cover w-full h-full"
-        />
-      </a>
-    </div>
+<div class="max-w-2xl space-y-8">
+  <SectionTitle
+    title="Contact"
+    subtitle="Reach out to connect, collaborate, and create together"
+  />
+  <div class="grid md:grid-cols-2 gap-x-16">
+    <p>
+      <strong>Name:</strong> Alex Valle<br /><strong>Status:</strong>
+      US Citizen<br /><strong>Location:</strong>
+      Tampa, FL
+    </p>
+    <p class="overflow-hidden text-ellipsis">
+      <strong>Clearance:</strong> Secret<br /><strong>Email:</strong>
+      alex.valle@gmail.com<br /><strong>Languages:</strong>
+      English, Español
+    </p>
   </div>
 </div>
